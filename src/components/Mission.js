703ded4d06f4bb1f1efc0604d03452a3db1e8154
Devon Mason <devonmmason@gmail.com>
2019-11-18 13:42:12 -0500

    paragraph

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -5,7 +5,7 @@ const Mission = () => {
         <div className="mainMission">
                 <h1 id="missionTitle">Track & Discover Offices Around the World</h1>
 
-                <p className="missionText"> Our mission is to become the number one source for tracking and finding offices around the globe. 
+                <p id="missionText"> Our mission is to become the number one source for tracking and finding offices around the globe. 
                <br />
                <br />
                Digital nomads are a rising trend. According to MBO partners annual report of State of Independence in America, 4.8 million workers describe themselves as digital nomads. With so many individuals embracing nomadic work, Travel N Code will be an integral tool for the worker on the go! </p>
