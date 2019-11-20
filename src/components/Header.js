6322b834037d6d6c9c8ad9af314441aaa634318f
Devon Mason <devonmmason@gmail.com>
2019-11-20 11:03:09 -0500

    with better routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import Mission from './Mission';
+import Main from './Main';
 import OfficeIndex from './offices/OfficeIndex'
 import { BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
 
@@ -8,10 +9,14 @@ import { BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
 const Header = (props) => {
 
     return (
+        
         <div className="header">
                 <div className="routing">
                     <div className = "routing-list-styling">
-                        <Link to="/officeindex">Office Index </Link>
+                    {/* <Link to="/">Home</Link> 
+                         &nbsp;
+                        &nbsp; */}
+                        <Link to="/">Office Index </Link>
                         &nbsp;
                         &nbsp;
                         <Link to="/mission">Mission</Link>
@@ -19,7 +24,8 @@ const Header = (props) => {
                 </div>
 
              <Switch>
-                 <Route exact path ="/officeindex"><OfficeIndex clickLogout ={props.clickLogout} updateToken={props.updateToken} token={props.token} setSessionToken={props.setSessionToken} /></Route>
+                 {/* <Route exact path="/"><Main clickLogout= {props.clickLogout} /></Route> */}
+                 <Route exact path ="/"><OfficeIndex clickLogout ={props.clickLogout} updateToken={props.updateToken} token={props.token} setSessionToken={props.setSessionToken} /></Route>
                  <Route exact path ="/mission"><Mission clickLogout ={props.clickLogout} /></Route>
             </Switch>  
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,13 @@ import {BrowserRouter as Router} from 'react-router-dom';
 const Main = (props) => {
     return (
        <div className="main">
+           
                 <Router>   
 
                 <Header className="navHeader" clickLogout ={props.clickLogout} updateToken={props.updateToken} token={props.token} setSessionToken={props.setSessionToken}/>
         
                 </Router>
+                {/* <h1>Is this my homepage? </h1> */}
               
        </div>         
     )
